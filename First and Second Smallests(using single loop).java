57409ff0db36be128a7f5880bc02e34602a70ffb
MUDIT CHAUHAN <93607579+Mudi13@users.noreply.github.com>
2025-01-12 13:35:02 +0530

    Update First and Second Smallests(using single loop).java

--- First and Second Smallests(using single loop).java
+++ First and Second Smallests(using single loop).java
@@ -11,15 +11,23 @@ class Main {
         for(int i=0; i<N; ++i){
             arr[i]=sc.nextInt();
         }
-        int min_element = Integer.MAX_VALUE;
+        int min_element = Integer.MAX_VALUE; 
         int sec_min = Integer.MAX_VALUE;
         for(int i=0; i<N; ++i){
             if(arr[i]<min_element){
-                //min_element=arr[i];
                 sec_min=min_element;
                 min_element=arr[i];
             }
+            else if(arr[i]!=min_element && arr[i]<sec_min){
+                sec_min=arr[i];
+            }
+        }
+
+        if(sec_min==Integer.MAX_VALUE);
+        {
+            min_element = -1;
+            sec_min = -1;
         }
         System.out.print(min_element+" "+sec_min);
 	}    
-}
+} 
